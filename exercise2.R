3b6939599b69538f5636d495c97c9766fddeaacf
Eric Eckert <eric95@uw.edu>
2016-02-03 15:16:41 -0800

    I did it guys

--- exercise2.R
+++ exercise2.R
@@ -5,11 +5,32 @@
 # code, and give a name to a repeated task.
 library(jsonlite)
 
-# Write a function that allows you to specify an artist, and returns the top 10 tracks of that artist
+base <- "https://api.spotify.com/v1/search?"
+search <- paste0("q=", "Nelly")
+type <- "&type=artist"
+query_url <- paste0(base, search, type)
+data <- fromJSON(query_url)
+id <- data$artists$items$id[1]
+id_query <- paste0("https://api.spotify.com/v1/artists/", id, "/top-tracks?country=US")
+artist_data <- fromJSON(id_query)
+track_data <- artist_data$tracks$name
 
+# Write a function that allows you to specify an artist, and returns the top 10 tracks of that artist
+get_top_10 <- function(artist) {
+  base <- "https://api.spotify.com/v1/search?"
+  search <- paste0("q=", artist)
+  type <- "&type=artist"
+  query_url <- paste0(base, search, type)
+  data <- fromJSON(query_url)
+  id <- data$artists$items$id[1]
+  id_query <- paste0("https://api.spotify.com/v1/artists/", id, "/top-tracks?country=US")
+  artist_data <- fromJSON(id_query)
+  track_data <- artist_data$tracks$name
+  return(track_data)
+}
 
 # What are the top 10 tracks by Nelly?
-
+Nelly_top_10 <- get_top_10("Nelly")
 
 ### Bonus ### 
 # Write a function that allows you to specify a search type (artist, album, etc.), and a string, 
